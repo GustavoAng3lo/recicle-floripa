4924565408c9f44a37ac62840b7357bd03238140
GustavoAng3lo <luis7gustavo.angelo@gmail.com>
2026-08-28 16:34:13 -0300

    feat: configuracao do banco em nuvem Neon e correcao de rotas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-undef, @typescript-eslint/no-require-imports, @typescript-eslint/no-unused-vars */
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('❌ ERRO AO CONECTAR NO BANCO:', err.message);
   } else {
-    console.log('✅ BANCO DE DADOS CONECTADO COM SUCESSO!');
+    console.log('✅ BANCO DE DADOS NA NUVEM CONECTADO COM SUCESSO!');
   }
 });
+
 module.exports = pool;
